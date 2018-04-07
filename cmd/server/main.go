b16e844b8d33bb25dd776b56bcf09b0b303d4cb3
Tony Yip <tony@opensource.hk>
2018-04-07 20:21:49 +0800

    Add more error handling and debug logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 	"os"
 
 	_ "github.com/joho/godotenv/autoload"
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 	"github.com/tonyhhyip/seau/pkg/server/setup"
 )
+
+func init() {
+	var verbose bool
+	flag.BoolVar(&verbose, "verbose", false, "Verbose for debug")
+	flag.Parse()
+	if verbose {
+		logrus.SetLevel(logrus.DebugLevel)
+		logrus.Debug("Enable Verbose")
+	}
+	setup.Bootstrap()
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	http.ListenAndServe(":"+port, setup.GetHandler())
+	if err := http.ListenAndServe(":"+port, setup.GetHandler()); err != nil {
+		panic(err)
+	}
 }
